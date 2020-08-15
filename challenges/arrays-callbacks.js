492a1e8bb3267104633e7139a89d18f40f8626a2
Roy-Weiss <67227056+Roy-Weiss@users.noreply.github.com>
2020-08-14 22:38:34 -0400

    prototypes

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -38,7 +38,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
-zooAnimals.forEach(function(arr) {
+zooAnimals.map(function(arr) {
     lowCaseAnimalNames.push(arr["animal_name"].toLowerCase())
     return lowCaseAnimalNames
 })
